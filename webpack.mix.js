3da872f3bd23066b46a65994b067231316d51e0c
MammutAlex <alexk.mammut@gmail.com>
2019-01-16 10:42:25 +0200

    Correct style

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,7 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('build/js/web.js', 'dist/js/app.js')
+mix.js('build/js/app.js', 'dist/js/app.js')
     .styles([
         'build/css/styles.css',
     ], 'dist/css/all.css');
