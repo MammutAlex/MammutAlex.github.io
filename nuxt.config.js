98e2fcb9c94f6e275b7d7f87cddcb15d040bcb40
Alex Kovalchuk <alex@cargofy.com>
2020-11-09 16:26:21 +0200

    Correct to nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,44 @@
+export default {
+  // Target (https://go.nuxtjs.dev/config-target)
+  target: 'static',
+
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'Alex Kovalchuk - Cargofy CTO',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: [
+  ],
+
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [
+  ],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/color-mode-module
+    "@nuxtjs/color-mode",
+    // Doc: https://github.com/nuxt-community/tailwindcss-module
+    "@nuxtjs/tailwindcss"
+  ],
+  tailwindcss: {
+    // add '~tailwind.config` alias
+    exposeConfig: true
+  },
+  colorMode: {
+    // remove -mode suffix for Tailwind Dark mode support
+    classSuffix: ""
+  }
+}
