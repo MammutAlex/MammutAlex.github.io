c52f8149dc67367bad6af83cc3eb98492323cf8d
MammutAlex <alex@cargofy.com>
2023-03-01 21:23:48 +0200

    Update cd

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from '/Users/alex/PhpstormProjects/MammutAlex.github.io/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-
-
-export default defuFn(inlineConfig)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,13 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    css: [
+        '@/assets/css/main.css',
+    ],
     modules: [
         '@nuxt/content',
         '@nuxtjs/tailwindcss',
         'nuxt-headlessui',
         '@nuxt/image-edge',
     ],
-    css: [
-        '@/assets/css/main.css',
-    ]
+    ssr: false
 })
